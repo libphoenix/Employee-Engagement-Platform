4a785dd5601ebd18921ce59cf0918e38a8de0266
Deepak patel <deepakpatel@Deepaks-MacBook-Air.local>
2021-05-09 17:39:35 +0530

    trained model

--- Sentimental-Analysis/Build.py
+++ Sentimental-Analysis/Build.py
@@ -1,70 +1,147 @@
-from DataCleaning import clean_and_load
-import pandas as pd
-import numpy as np
-import constants as cn
-
+from tensorflow.keras import datasets
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import constants as con
+from tensorflow.keras import Sequential, layers, optimizers
+from tensorflow import nn
 
 print("Program Started")
+
+
+# getting data from keras
+print("Getting data from keras...")
+(train_data, train_labels), (test_data, test_lables) = datasets.imdb.load_data(num_words = con.vocab_size)
+
+# word index 
+
+word_index = datasets.imdb.get_word_index()
+
+word_index = {k: (v+ 3) for k,v in word_index.items()}
+word_index["<PAD>"] = 0 
+word_index["<START>"] = 1
+word_index["<UNK>"] = 2 # unknown
+word_index["<UNUSED>"] = 3
+
+print(".... done")
+
+# Pre-processing 
+print("Preprocessing started....")
+train_data = pad_sequences(train_data, 
+                            value = word_index["<PAD>"], 
+                            padding = 'post', 
+                            maxlen = con.max_length )
+
+test_data = pad_sequences(test_data,
+                            value = word_index["<PAD>"],
+                            padding = 'post',
+                            maxlen = con.max_length )
+
+print(".... done")
+
+print("Bulding model....")
+
+model = Sequential()
+model.add(layers.Embedding(con.vocab_size, 16))
+model.add(layers.GlobalAveragePooling1D())
+model.add(layers.Dense(16, activation = nn.relu))
+model.add(layers.Dense(1, activation = nn.sigmoid))
+
+print(model.summary())
+
+model.compile(optimizer = optimizers.Adam(), 
+                loss = 'binary_crossentropy',
+                metrics = ['accuracy'])
+print("building done")
+
+x_val = train_data[:con.validation]
+partial_x_train = train_data[con.validation:]
+
+y_val = train_labels[:con.validation]
+partial_y_train = train_labels[con.validation:]
+
+input("Press any key to train model")
+
+model.fit(partial_x_train, 
+            partial_y_train, 
+            epochs = con.num_epochs, 
+            batch_size = 512,
+            validation_data = (x_val, y_val))
+
+model.save("trained_model")
+
+print("training done")
+print("evaluating model")
+res = model.evaluate(test_data, test_lables)
+print(res)
+
+# from DataCleaning import clean_and_load
+# import pandas as pd
+# import numpy as np
+# import constants as cn
+# from tensorflow.keras import datasets
+
+
+# print("Program Started")
 # loadng data
-print("File loading...", end = "&")
-data_type = {'clean_text': str, 'category': int}
-datastore = clean_and_load(cn.file_path, data_type)
-print("... done")
+# print("File loading...", end = "")
+# data_type = {'clean_text': str, 'category': int}
+# datastore = clean_and_load(cn.file_path, data_type)
+# print("... done")
 
 #spliting dataset 
-print("Spliting dataset....",end = "&")
-training_set = int(datastore.shape[0] * cn.training_percent)
+# print("Spliting dataset....")
+# training_set = int(datastore.shape[0] * cn.training_percent)
 
-training_df = datastore[:training_set]
-testing_df = datastore[training_set:]
-training_sentences = training_df.clean_text.to_numpy()
-training_lables = training_df.category.to_numpy()
+# training_df = datastore[:training_set]
+# testing_df = datastore[training_set:]
+# training_sentences = training_df.clean_text.to_numpy()
+# training_lables = training_df.category.to_numpy()
 
-testing_sentences = testing_df.clean_text.to_numpy()
-testing_lables = testing_df.category.to_numpy()
-print(".... done")
+# testing_sentences = testing_df.clean_text.to_numpy()
+# testing_lables = testing_df.category.to_numpy()
+
 
 # data preprocessing
-print("Data per-processing....", end = "&")
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+# print("Data per-processing....", end = "")
+# from tensorflow.keras.preprocessing.text import Tokenizer
+# from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-tokenizer = Tokenizer(oov_token = '<oov>', lower = False, split = ' ')
-tokenizer.fit_on_texts(training_sentences)
+# tokenizer = Tokenizer(oov_token = '<oov>', lower = False, split = ' ', num_words = cn.vocab_size)
+# tokenizer.fit_on_texts(training_sentences)
 
-training_sequence = tokenizer.texts_to_sequences(training_sentences)
-training_padded = pad_sequences(training_sequence, maxlen = cn.max_length,padding = 'post', truncating = 'post')
+# training_sequence = tokenizer.texts_to_sequences(training_sentences)
+# training_padded = pad_sequences(training_sequence, maxlen = cn.max_length,padding = 'post', truncating = 'post')
 
-testing_sequence = tokenizer.texts_to_sequences(testing_sentences)
-testing_padded = pad_sequences(testing_sequence, maxlen = cn.max_length,padding = 'post', truncating = 'post')
+# testing_sequence = tokenizer.texts_to_sequences(testing_sentences)
+# testing_padded = pad_sequences(testing_sequence, maxlen = cn.max_length,padding = 'post', truncating = 'post')
 
-training_padded = np.array(training_padded)
-training_lables = np.array(training_lables)
-testing_padded = np.array(testing_padded)
-testing_lables = np.array(testing_lables)
-print(".... done")
+# training_padded = np.array(training_padded)
+# training_lables = np.array(training_lables)
+# testing_padded = np.array(testing_padded)
+# testing_lables = np.array(testing_lables)
+# print(".... done")
 
 #Building model 
-print("Creating model....")
-from tensorflow.keras import layers, models
+# print("Creating model....")
+# from tensorflow.keras import layers, models
 
-model = models.Sequential([
-    layers.Embedding(input_dim = len(tokenizer.word_index),output_dim = cn.embedding_dim, input_length = cn.max_length),
-    layers.GlobalAveragePooling1D(),
-    layers.Dense(24,activation = 'relu'),
-    layers.Dense(3, activation= 'relu')
-    ])
+# model = models.Sequential([
+#     layers.Embedding(input_dim = cn.vocab_size,output_dim = cn.embedding_dim, input_length = cn.max_length),
+#     layers.GlobalAveragePooling1D(),
+#     layers.Dense(24,activation = 'relu'),
+#     layers.Dense(3, activation= 'relu')
+#     ])
 
-model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+# model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
 
-model.summary()
+# model.summary()
 
-print(".... Done")
+# print(".... done")
 
 # Traning model
-print("Training model....")
+# input("Press any key to start training model...")
+# print("Training model....")
 
-model.fit(training_padded, training_lables, epochs = cn.num_epochs, validation_data = (testing_padded, testing_lables))
-print("...Training done")
+# model.fit(training_padded, training_lables, epochs = cn.num_epochs, validation_data = (testing_padded, testing_lables), verbose = 2)
+# print("...Training done")
 
-model.save("Trained_model")
+# model.save("Trained_model")
--- Sentimental-Analysis/constants.py
+++ Sentimental-Analysis/constants.py
@@ -1,9 +1,9 @@
 
 # global constants
 embedding_dim = 16
-max_length = 100
+max_length = 256
 file_path = "Data/Twitter_Data.csv"
-sentences = []
-lables = []
 training_percent = 0.8
-num_epochs = 30
+num_epochs = 50
+vocab_size = 200000
+validation = 10000
