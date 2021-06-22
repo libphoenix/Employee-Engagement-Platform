b6ec3db63e021a2508089a72f65a5db812f61d39
Deepak patel <deepakpatelg16@gmail.com>
2021-06-22 13:18:15 +0530

    added new makefile for sentimental analysis

--- SentimentalAnalysis/main.py
+++ SentimentalAnalysis/main.py
@@ -6,9 +6,15 @@ from MachineLearning.Build import train_model
 from UI.Interface import App
 
 
-if __name__ == '__main__':
-    # train_model()
+value = sys.argv[1]
+
+if value == "train":
+    train_model()
+elif value == "gui":
     app = QApplication(sys.argv)
     ex = App()
     sys.exit(app.exec_())
-    # predict("This is awesome so loving movie.")
+elif value == "perdict":
+    print("Enter a review: \n")
+    text = input()
+    print(predict(text))
